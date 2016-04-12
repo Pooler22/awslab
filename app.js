659d2c7a86e37683d66d3b91e432d475e042fa74
Ubuntu <rudypawel12@gmail.com>
2016-04-12 09:13:50 +0000

    lab 1 zad 3

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 
 var lab1_1 = require("./lab/lab1_1").lab
+var create = require("./lab/create").lab
+var lab1_2 = require("./lab/lab1_2").lab
 var example_1 = require("./example_1").lab;
-
 
 var PORT = 8080;
 
@@ -10,8 +11,10 @@ var PORT = 8080;
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},
-       
-	{path: "/example_1", action: example_1}, 
+       {path: "/info", action: lab1_2},
+
+	{path: "/example_1", action: example_1},
+	{path: "/create", action: create}, 
 	];
 
 var service = require("./lib/service").http(urlMap);
